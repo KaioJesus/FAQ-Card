de44f6534f5d37c637e5d219a0ff77a06b2dee99
Kaio Jesus <kaioramos990@hotmail.com>
2022-10-25 18:17:01 -0300

    add button and functions to the button, but still not working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-const clicarshow = document.querySelector('card-ask__answer-p-question');
-const pergunta = document.querySelector('card-ask__answer-p-ask')
+const clicarshow = document.querySelectorAll('.card-ask__answer-p-answer');
+const pergunta = document.querySelector('.card-ask__answer-p-ask')
 
-function askFaq (){
-    onlick = clicarshow.style.display = 'block';
+
+const botaoshow = document.querySelectorAll('.showMessage')
+
+for(let i = 0; i < botaoshow.length; i++){
+    const botao = botaoshow[i];
+    
+    botaoshow.onclick = function(){
+        clicarshow.style.display = 'block';
+        // if (botaoshow.onclick == 'ativa'){
+        //     botaoshow.onclick = cliclarshow.style.display ='none';
+        // }
+    }
 }
